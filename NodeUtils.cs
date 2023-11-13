b69e7eee1f1076884d2e89967cd262c511509cb5
jponzo <jeremy.ponzo@gmail.com>
2023-11-13 21:44:29 +0100

    add findNodesInParents

--- NodeUtils.cs
+++ NodeUtils.cs
@@ -4,13 +4,25 @@ using System.Collections.Generic;
 
 public class NodeUtils
 {
-    public static List<T> FindNodeInChilds<T>(Node node) where T : Node
+    public static List<T> FindNodesInChildren<T>(Node node) where T : Node
     {
         List<T> tNodes = new List<T>();
 
         if (node is T) tNodes.Add((T)node);
         foreach(Node child in node.GetChildren())
-            tNodes.AddRange(FindNodeInChilds<T>(child));
+            tNodes.AddRange(FindNodesInChildren<T>(child));
+
+        return tNodes;
+    }
+
+    public static List<T> FindNodesInParents<T>(Node node) where T : Node
+    {
+        List<T> tNodes = new List<T>();
+        if (node is T) tNodes.Add((T)node);
+
+        Node parent = node.GetParent();
+        if (parent != null)
+            tNodes.AddRange(FindNodesInParents<T>(parent));
 
         return tNodes;
     }
