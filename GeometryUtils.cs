7bc5b3a56683b0c7c1c0f5a47b2a5073e8fcabac
jponzo <jeremy.ponzo@gmail.com>
2024-02-16 19:51:38 +0100

    add geometry utils

---
+++ GeometryUtils.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public class GeometryUtils
+{
+    public static Vector2 ClampInCircle(Vector2 location, Vector2 center, float radius)
+    {
+        if (location.DistanceTo(center) < radius) return location;
+
+        return center + (location - center).Normalized() * radius;
+    }
+
+    public static bool IsLocationInCone(Vector2 location, Vector2 coneOrigin, Vector2 coneDircection, float coneHalfAngle, float coneRange)
+    {
+        if (!GeometryUtils.IsLocationInCircle(location, coneOrigin, coneRange))
+            return false;
+
+        float angle = Mathf.Abs(Mathf.RadToDeg(coneDircection.AngleTo((location - coneOrigin).Normalized())));
+        return angle < coneHalfAngle;
+    }
+
+    public static bool IsLocationInCircle(Vector2 location, Vector2 center, float radius)
+    {
+        return location.DistanceTo(center) < radius;
+    }
+}
