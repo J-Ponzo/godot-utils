f1ce2f262e2c6d60cfee92639e42dcb313f93fde
jponzo <jeremy.ponzo@gmail.com>
2024-05-01 10:00:41 +0200

    Add AudioUtils.DbToLinear

--- AudioUtils.cs
+++ AudioUtils.cs
@@ -8,4 +8,10 @@ public class AudioUtils
         float Db = 20f * Mathf.Log(linear);
         return Db;
     }
+
+    public static float DbToLinear(float db)
+    {
+        float linear = Mathf.Pow(10f, db / 20f);
+        return linear;
+    }
 }
